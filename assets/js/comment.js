775ac7e86857ddf97ff104b31bc0303caf02c862
Quốc Định <pqd1997@gmail.com>
2018-07-12 16:17:06 +0700

    comment

---
+++ assets/js/comment.js
@@ -0,0 +1,25 @@
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=154259861915442&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121882571-1');
+
+
+
+
+(adsbygoogle = window.adsbygoogle || []).push({
+  google_ad_client: "ca-pub-3145178805367189",
+  enable_page_level_ads: true
+});
